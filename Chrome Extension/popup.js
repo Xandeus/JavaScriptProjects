0e11eb02ffbdc2a0f53b770acaae7970bd493c0b
Alex McKee <alexander.mckeever@gmail.com>
2017-09-24 20:13:42 -0500

    Improves github webpage

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,6 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tabs){
   var newURL = "http://reddit.com/r/random/top";
   var newURL2 = "popup.html";
-  for(var i = 0;i<5;i++)
+  for(var i = 0;i<5;i++){
 	chrome.tabs.create({ url: newURL });
+  }
+  chrome.tabs.create({ url: newURL2 });
 });
